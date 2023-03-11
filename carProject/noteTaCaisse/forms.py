f239d2f56029abe2499ffa0f65c0b62ffc6ec20a
MasWap <lilian.layrac@gmail.com>
2023-03-11 19:22:20 +0100

    removing exemple project

--- carProject/noteTaCaisse/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class VoteForm(forms.Form):
-    vote = forms.IntegerField(label='Vote', min_value=0, max_value=5)
--- carProject/noteTaCaisse/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import reset_vote_count_view
-from . import views
-
-urlpatterns = [
-    path('', views.car_list, name="car_list"),
-    path('car_detail/<int:car_id>/', views.car_detail, name='car_detail'),
-    path('reset_vote_count/<int:car_id>/', views.reset_vote_count_view, name='reset_vote_count'),
-]
